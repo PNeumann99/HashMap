2701e7799bbedbabdbc4dffc9eaadfd009b17157
Paul <p.neumann76@gmail.com>
2021-06-28 14:41:40 +0200

    Initial commit

---
+++ src/HashMap.java
@@ -0,0 +1,22 @@
+public class HashMap<Key, Value>{
+    private static final int m = 997;
+    private HashMapEntry<Key, Value>[] buckets;
+    private int size = 0;
+
+
+    public void put(Key key, Value value){
+
+    }
+
+    public boolean contains(Key key){
+
+    }
+
+    public Value get(Key key){
+
+    }
+
+    public void delete(Key key){
+
+    }
+}
